c1e4193612a285f5b39903e4bb4870a1221b84d6
Billy <billypatrick767@gmail.com>
2022-08-16 12:54:42 +0300

    fix: get Member query

--- opensrp-chw-hivst/src/main/java/org/smartregister/chw/hivst/dao/HivstDao.java
+++ opensrp-chw-hivst/src/main/java/org/smartregister/chw/hivst/dao/HivstDao.java
@@ -21,6 +21,7 @@ public class HivstDao extends AbstractDao {
 
         return res.get(0);
     }
+
     public static Date getHivstFollowUpVisitDate(String baseEntityID) {
         String sql = "SELECT eventDate FROM event where eventType ='Hivst Follow-up Visit' AND baseEntityId ='" + baseEntityID + "'";
 
@@ -32,6 +33,7 @@ public class HivstDao extends AbstractDao {
 
         return res.get(0);
     }
+
     public static void closeHivstMemberFromRegister(String baseEntityID) {
         String sql = "update ec_hivst_confirmation set is_closed = 1 where base_entity_id = '" + baseEntityID + "'";
         updateDB(sql);
@@ -66,7 +68,39 @@ public class HivstDao extends AbstractDao {
     }
 
     public static MemberObject getMember(String baseEntityID) {
-        String sql = "select m.base_entity_id , m.unique_id , m.relational_id , m.dob , m.first_name , m.middle_name , m.last_name , m.gender , m.phone_number , m.other_phone_number , f.first_name family_name ,f.primary_caregiver , f.family_head , f.village_town ,fh.first_name family_head_first_name , fh.middle_name family_head_middle_name , fh.last_name family_head_last_name, fh.phone_number family_head_phone_number , ancr.is_closed anc_is_closed, pncr.is_closed pnc_is_closed, pcg.first_name pcg_first_name , pcg.last_name pcg_last_name , pcg.middle_name pcg_middle_name , pcg.phone_number  pcg_phone_number , mr.* from ec_family_member m inner join ec_family f on m.relational_id = f.base_entity_id inner join ec_hivst_confirmation mr on mr.base_entity_id = m.base_entity_id left join ec_family_member fh on fh.base_entity_id = f.family_head left join ec_family_member pcg on pcg.base_entity_id = f.primary_caregiver left join ec_anc_register ancr on ancr.base_entity_id = m.base_entity_id left join ec_pregnancy_outcome pncr on pncr.base_entity_id = m.base_entity_id where m.base_entity_id ='" + baseEntityID + "' ";
+        String sql = "select m.base_entity_id, " +
+                "       m.unique_id, " +
+                "       m.relational_id, " +
+                "       m.dob, " +
+                "       m.first_name, " +
+                "       m.middle_name, " +
+                "       m.last_name, " +
+                "       m.gender, " +
+                "       m.phone_number, " +
+                "       m.other_phone_number, " +
+                "       f.first_name     family_name, " +
+                "       f.primary_caregiver, " +
+                "       f.family_head, " +
+                "       f.village_town, " +
+                "       fh.first_name    family_head_first_name, " +
+                "       fh.middle_name   family_head_middle_name, " +
+                "       fh.last_name     family_head_last_name, " +
+                "       fh.phone_number  family_head_phone_number, " +
+                "       ancr.is_closed   anc_is_closed, " +
+                "       pncr.is_closed   pnc_is_closed, " +
+                "       pcg.first_name   pcg_first_name, " +
+                "       pcg.last_name    pcg_last_name, " +
+                "       pcg.middle_name  pcg_middle_name, " +
+                "       pcg.phone_number pcg_phone_number, " +
+                "       mr.* " +
+                "from ec_family_member m " +
+                "         inner join ec_family f on m.relational_id = f.base_entity_id " +
+                "         inner join ec_hiv_register mr on mr.base_entity_id = m.base_entity_id " +
+                "         left join ec_family_member fh on fh.base_entity_id = f.family_head " +
+                "         left join ec_family_member pcg on pcg.base_entity_id = f.primary_caregiver " +
+                "         left join ec_anc_register ancr on ancr.base_entity_id = m.base_entity_id " +
+                "         left join ec_pregnancy_outcome pncr on pncr.base_entity_id = m.base_entity_id " +
+                "where m.base_entity_id = '" + baseEntityID + "' ";
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
 
         DataMap<MemberObject> dataMap = cursor -> {
