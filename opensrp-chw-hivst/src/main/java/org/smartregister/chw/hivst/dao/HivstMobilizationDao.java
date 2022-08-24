c35ae933385817e550534936ef45ac69ebfffc25
Billy <billypatrick767@gmail.com>
2022-08-24 16:07:03 +0300

    fix: update the dao for creating records for Mobilization

--- opensrp-chw-hivst/src/main/java/org/smartregister/chw/hivst/dao/HivstMobilizationDao.java
+++ opensrp-chw-hivst/src/main/java/org/smartregister/chw/hivst/dao/HivstMobilizationDao.java
@@ -7,19 +7,37 @@ import org.smartregister.dao.AbstractDao;
 
 import java.util.List;
 
+
 public class HivstMobilizationDao extends AbstractDao {
 
-    //TODO: Compute condoms issued
-    public static List<HivstMobilizationModel> getMobilizationSessions() {
-        String sql = "SELECT * FROM " + Constants.TABLES.HIVST_MOBILIZATION;
+    public static void updateData(String baseEntityID, String mobilization_date, String female_clients_reached, String male_clients_reached, String male_condoms_issued, String female_condoms_issued) {
+        String sql = "INSERT INTO ec_hivst_mobilization " +
+                "           (id, mobilization_date, female_clients_reached, male_clients_reached, male_condoms_issued, female_condoms_issued) " +
+                "           VALUES (" +
+                "                   '" + baseEntityID + "', " +
+                "                   '" + mobilization_date + "', " +
+                "                   '" + female_clients_reached + "', " +
+                "                   '" + male_clients_reached + "', " +
+                "                   '" + male_condoms_issued + "', " +
+                "                   '" + female_condoms_issued + "') " +
+                "              " +
+                " ON CONFLICT (id) DO UPDATE SET mobilization_date      = '" + mobilization_date + "',      " +
+                "                               female_clients_reached = '" + female_clients_reached + "',  " +
+                "                               male_clients_reached   = '" + male_clients_reached + "',    " +
+                "                               male_condoms_issued    = '" + male_condoms_issued + "',     " +
+                "                               female_condoms_issued  = '" + female_condoms_issued + "'    ";
+        updateDB(sql);
+    }
 
-        AbstractDao.DataMap<HivstMobilizationModel> dataMap = cursor -> {
+    public static List<HivstMobilizationModel> getMobilizationSessions() {
+        String sql = "SELECT * FROM " + Constants.TABLES.HIVST_MOBILIZATION + " ORDER BY mobilization_date DESC";
+
+        DataMap<HivstMobilizationModel> dataMap = cursor -> {
             HivstMobilizationModel hivstMobilizationModel = new HivstMobilizationModel();
             hivstMobilizationModel.setSessionId(cursor.getString(cursor.getColumnIndex(DBConstants.KEY.BASE_ENTITY_ID)));
-            hivstMobilizationModel.setSessionDate(cursor.getString(cursor.getColumnIndex(Constants.FORM_SUBMISSION_FIELD.MOBILIZATION_DATE)));
-            hivstMobilizationModel.setSessionParticipants(cursor.getString(cursor.getColumnIndex(Constants.FORM_SUBMISSION_FIELD.MOBILIZATION_PARTICIPANTS_NUMBER)));
-            hivstMobilizationModel.setCondomsIssued(cursor.getString(cursor.getColumnIndex(Constants.FORM_SUBMISSION_FIELD.MOBILIZATION_MALE_CONDOMS_ISSUED)));
-
+            hivstMobilizationModel.setSessionDate(cursor.getString(cursor.getColumnIndex(DBConstants.KEY.MOBILIZATION_DATE)));
+            hivstMobilizationModel.setSessionParticipants(computeSessionParticipants(cursor.getString(cursor.getColumnIndex(DBConstants.KEY.FEMALE_CLIENTS_REACHED)), cursor.getString(cursor.getColumnIndex(DBConstants.KEY.MALE_CLIENTS_REACHED))));
+            hivstMobilizationModel.setCondomsIssued(computeCondomsIssued(cursor.getString(cursor.getColumnIndex(DBConstants.KEY.FEMALE_CONDOMS_ISSUED)), cursor.getString(cursor.getColumnIndex(DBConstants.KEY.MALE_CONDOMS_ISSUED))));
             return hivstMobilizationModel;
         };
 
@@ -29,9 +47,15 @@ public class HivstMobilizationDao extends AbstractDao {
         return res;
     }
 
-    //TODO Refactor query
-    public static void updateData(String baseEntityID, String sbccDate, String locationType, String participantsNumber) {
-        String sql = String.format("INSERT INTO ec_sbcc (id, sbcc_date, location_type, participants_number) VALUES ('%s', '%s', '%s', '%s') ON CONFLICT (id) DO UPDATE SET sbcc_date = '%s', location_type = '%s', participants_number = '%s'", baseEntityID, sbccDate, locationType, participantsNumber, sbccDate, locationType, participantsNumber);
-        updateDB(sql);
+    private static String computeSessionParticipants(String femaleParticipants, String maleParticipants) {
+        int sum = Integer.parseInt(femaleParticipants) + Integer.parseInt(maleParticipants);
+        return String.valueOf(sum);
     }
+
+    private static String computeCondomsIssued(String femaleCondoms, String maleCondoms) {
+        int sum = Integer.parseInt(femaleCondoms) + Integer.parseInt(maleCondoms);
+        return String.valueOf(sum);
+    }
+
+
 }
